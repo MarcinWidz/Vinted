f4d17b7fe482fb8f27cb49a5f9d7714dd22eb4c2
Marcin Widz <marcinwidz@MacBook-Air-de-Marcin.local>
2021-07-11 17:47:16 +0200

    update

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require("express");
 const formidable = require("express-formidable");
 const mongoose = require("mongoose");
 const cors = require("cors");
-app.use(cors());
 require("dotenv").config();
 
 const app = express();
+app.use(cors());
 app.use(formidable());
 
 mongoose.connect(process.env.MONGODB_URI, {
